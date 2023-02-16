e3b9199eb5d299b9002d4b50aad2ec92b5db5527
Younseo Jang <younseojj@gmail.com>
2023-02-16 20:10:16 +0900

    initial commit

---
+++ src/main/java/com/gorgeouspotatoes/cherryblossom/CherryblossomApplication.java
@@ -0,0 +1,13 @@
+package com.gorgeouspotatoes.cherryblossom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryblossomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryblossomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorgeouspotatoes/cherryblossom/CherryblossomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorgeouspotatoes.cherryblossom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryblossomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
